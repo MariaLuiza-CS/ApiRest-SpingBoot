ffcb18e3328905aa92c6c04a8e91a8d3ccfa54d0
MariaLuiza-CS <m.luiza1843@gmail.com>
2021-04-27 15:58:20 -0300

    primeiro commit do projeto

---
+++ PersonApi/src/test/java/com/digitalinnovation/PersonApi/PersonApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.digitalinnovation.PersonApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
